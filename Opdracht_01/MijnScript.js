f3438b1649b57dbac8a6c332b452a373450fc139
MichaëlVL <60927341+Fyera@users.noreply.github.com>
2020-04-21 14:53:55 +0200

    Update MijnScript.js

--- Opdracht_01/MijnScript.js
+++ Opdracht_01/MijnScript.js
@@ -2,7 +2,6 @@ $('#myText').on('focus', function() {
     $('#myText').val('');
 
     $('#myText').on('keyup', validateMaxLength);
-
 });
 
 function validateMaxLength() {
@@ -10,13 +9,10 @@ function validateMaxLength() {
     var maxlength = $(this).data('maxlength');
     var textLengte = text.length;
     var rest = maxlength - textLengte;
-    console.log(textLengte);
 
     if (textLengte >= maxlength) {
-        console.log("groter");
         $('p#rest').text("Je zit over het maximaal aantal tekens.");
     } else {
-        console.log("kleinre");
         $('p#rest').text("resterende tekens: " + rest);
     }
 };
