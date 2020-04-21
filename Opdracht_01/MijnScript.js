ae41e97a08f9fa3e46572e50ee4ac58957501348
Fyera <michaelvanlaer@gmail.com>
2020-04-21 14:52:04 +0200

    Opdracht 01
    button geadd

--- Opdracht_01/MijnScript.js
+++ Opdracht_01/MijnScript.js
@@ -1,10 +1,22 @@
-$('#myText').keyup(validateMaxLength);
+$('#myText').on('focus', function() {
+    $('#myText').val('');
+
+    $('#myText').on('keyup', validateMaxLength);
+
+});
 
 function validateMaxLength() {
     var text = $(this).val();
     var maxlength = $(this).data('maxlength');
+    var textLengte = text.length;
+    var rest = maxlength - textLengte;
+    console.log(textLengte);
 
-    if (maxlength > 0) {
-        $(this).val(text.substr(0, maxlength));
+    if (textLengte >= maxlength) {
+        console.log("groter");
+        $('p#rest').text("Je zit over het maximaal aantal tekens.");
+    } else {
+        console.log("kleinre");
+        $('p#rest').text("resterende tekens: " + rest);
     }
-}
+};
